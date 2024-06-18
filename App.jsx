5b65e10ca093a0c06d0f8f7d6b256b5d4d46d661
FuzoiX <163722520+FuzoiX@users.noreply.github.com>
2024-06-18 16:38:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import AppCompo from "./components/AppCompo"
+import NavBar from "./components/Nav"
+import StartingPage from "./components/StartingPage"
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+
+      <AppCompo />
+
+    </>
+  )
+}
+
+export default App
